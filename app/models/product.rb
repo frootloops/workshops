2d7a7b8bcdd4dcfce64de9f800248fd9a39fb4dd
Arsen Gasparyan <sir.frootloops@gmail.com>
2015-03-26 20:59:06 +0300

    Fix #4: add validates to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, :description, :price, presence: true
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+    reviews.average(:rating)
+  end
 end
